9d6c1efa524f97c1913b4896ffdbe2cc021a5686
Henk Boelman <henk@boelman.eu>
2017-05-18 12:37:40 +0200

    Sample Code

---
+++ CustomVisionApiApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CustomVisionApiApp
+{
+    static class Program
+    {
+        private static string _predictionKey;
+        private static string _predictionEndpointUrl;
+
+        static void Main()
+        {
+            _predictionKey = "[Insert Prediction Key]";
+            _predictionEndpointUrl = "[Insert Prediction Endpoint]";
+
+            Console.Write("Enter image path: ");
+            string filePath = Console.ReadLine();
+
+            byte[] byteData = GetFileAsByteArray(filePath);
+            MakeRequest(byteData).Wait();
+
+            Console.WriteLine("\nHit any key to exit");
+            Console.ReadLine();
+        }
+
+        static async Task MakeRequest(byte[] byteData)
+        {
+            using (var content = new ByteArrayContent(byteData))
+            {
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Prediction-Key", _predictionKey);
+
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                HttpResponseMessage response = await client.PostAsync(_predictionEndpointUrl, content);
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        static byte[] GetFileAsByteArray(string filePath)
+        {
+            FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+    }
+}
---
+++ CustomVisionSdkApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Microsoft.Cognitive.CustomVision;
+using Microsoft.Cognitive.CustomVision.Models;
+
+namespace CustomVisionSdkApp
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Guid projectId = Guid.Parse("[Insert project ID]");
+            string predictionKey = "[Insert prediction Key]";
+            string imagePath = @"d:\sample.jpg";
+
+            MemoryStream testImage = new MemoryStream(File.ReadAllBytes(imagePath));
+
+            PredictionEndpointCredentials predictionEndpointCredentials = new PredictionEndpointCredentials(predictionKey);
+            PredictionEndpoint endpoint = new PredictionEndpoint(predictionEndpointCredentials);
+
+            ImagePredictionResultModel result = endpoint.PredictImage(projectId, testImage);
+
+            // Loop over each prediction and write out the results
+            foreach (ImageTagPrediction prediction in result.Predictions)
+            {
+                Console.WriteLine($"\t{prediction.Tag}: {prediction.Probability:P1}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
